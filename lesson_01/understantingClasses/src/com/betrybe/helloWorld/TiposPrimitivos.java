08cf54fcb35fe7b8cb597173e918c0163876ac5e
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-02-21 20:24:10 -0300

    feat: create new class TiposPrimitivos

--- lesson_01/understantingClasses/src/com/betrybe/helloWorld/TiposPrimitivos.java
+++ lesson_01/understantingClasses/src/com/betrybe/helloWorld/TiposPrimitivos.java
@@ -1,2 +1,10 @@
-package com.betrybe.helloWorld;public class TiposPrimitivos {
+package com.betrybe.helloWorld;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        short num = 300;
+        short formatadaComUndescore = 20_000; // variavel
+        System.out.println(num);
+        System.out.println(formatadaComUndescore); // 200000
+    }
 }
