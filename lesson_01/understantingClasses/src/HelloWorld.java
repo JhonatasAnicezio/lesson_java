e74937d13705fbb9b8c8bf33ea9b8f4cc39cb639
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-02-21 15:41:35 -0300

    feat: create repository of the following class 1.2

---
+++ lesson_01/understantingClasses/src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Primeira Linha");
+        System.out.println("Segunda Linha");
+
+        System.out.println("Primeira Linha");
+        System.out.println("Será impresso na mesma linha");
+    }
+}
