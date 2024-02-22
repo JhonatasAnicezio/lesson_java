196a1b668badf2857396af2ee5dc0ef719f0f173
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-02-21 21:03:49 -0300

    feat: create new class ObjetosString

---
+++ lesson_01/understantingClasses/src/com/betrybe/helloWorld/ObjetosString.java
@@ -0,0 +1,9 @@
+package com.betrybe.helloWorld;
+
+public class ObjetosString {
+    public static void main(String[] args) {
+        String frase = "Estou aprendendo Java!";
+        String numeros = "09890";
+        String caractere = "a";
+    }
+}
