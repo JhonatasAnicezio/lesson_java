fb5d1cc15e403e7bf6cc389ecc0a46a5fb28cb2b
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-15 13:44:00 -0300

    feat: instantiading atributes for new modified

--- lesson_02/AbstractionAndEncapsulation/src/main/java/com/betrybe/eletronic/Aplication.java
+++ lesson_02/AbstractionAndEncapsulation/src/main/java/com/betrybe/eletronic/Aplication.java
@@ -5,8 +5,11 @@ public class Aplication {
   public static void main(String[] args) {
     Television television = new Television("Phillips","LCD", 32);
 
-    television.tunOn();
-    television.turnOff();
+    System.out.println("A televisão está ligada?");
+    System.out.println(television.isOn());
+
+    System.out.println("O volume atual é:");
+    System.out.println(television.getVolume());
 
     System.out.println(television.info());
   }
