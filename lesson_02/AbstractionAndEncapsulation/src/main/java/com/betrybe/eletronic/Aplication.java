70dbe72257b2583d465dfe0f432ad844fa6f49be
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-15 11:29:35 -0300

    feat: implements Aplication class by adding television instantiation

--- lesson_02/AbstractionAndEncapsulation/src/main/java/com/betrybe/eletronic/Aplication.java
+++ lesson_02/AbstractionAndEncapsulation/src/main/java/com/betrybe/eletronic/Aplication.java
@@ -2,4 +2,11 @@ package com.betrybe.eletronic;
 
 public class Aplication {
 
+  public static void main(String[] args) {
+    System.out.println("Iniciando o sistema...");
+
+    Television television = new Television();
+
+    System.out.println("Finalizando sistema...");
+  }
 }
