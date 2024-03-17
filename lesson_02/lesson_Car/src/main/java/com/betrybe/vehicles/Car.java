eaedbf412c43f62f4e1fbd51391368f587b1f52e
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-17 12:13:04 -0300

    feat: create methods getters

--- lesson_02/lesson_Car/src/main/java/com/betrybe/vehicles/Car.java
+++ lesson_02/lesson_Car/src/main/java/com/betrybe/vehicles/Car.java
@@ -1,9 +1,9 @@
 package com.betrybe.vehicles;
 
 public class Car {
-  String MARCA;
-  String MODELO;
-  int ANO;
+  private String MARCA;
+  private String MODELO;
+  private int ANO;
 
   public Car(String MARCA, String MODELO, int ANO) {
     this.MARCA = MARCA;
@@ -14,4 +14,17 @@ public class Car {
   public void exibirInformacoes() {
     System.out.println("Marca: " + MARCA + ", Modelo: " + MODELO + ", Ano: " + ANO );
   }
+
+  public String getMARCA() {
+    return MARCA;
+  }
+
+  public String getMODELO() {
+    return MODELO;
+  }
+
+  public int getANO() {
+    return ANO;
+  }
+
 }
