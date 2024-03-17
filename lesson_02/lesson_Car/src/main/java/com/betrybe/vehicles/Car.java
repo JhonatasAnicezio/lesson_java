bdc1f356bc2f023017c29a26f4debfa5af929a69
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-17 12:10:55 -0300

    feat: implements class car

--- lesson_02/lesson_Car/src/main/java/com/betrybe/vehicles/Car.java
+++ lesson_02/lesson_Car/src/main/java/com/betrybe/vehicles/Car.java
@@ -1,5 +1,17 @@
 package com.betrybe.vehicles;
 
 public class Car {
+  String MARCA;
+  String MODELO;
+  int ANO;
 
+  public Car(String MARCA, String MODELO, int ANO) {
+    this.MARCA = MARCA;
+    this.MODELO = MODELO;
+    this.ANO = ANO;
+  }
+
+  public void exibirInformacoes() {
+    System.out.println("Marca: " + MARCA + ", Modelo: " + MODELO + ", Ano: " + ANO );
+  }
 }
