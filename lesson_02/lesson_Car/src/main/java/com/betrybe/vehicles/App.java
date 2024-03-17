9ae555ce88e41814582c8cc8af47922183ff7222
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-17 14:31:59 -0300

    feat: instantiating class Car in App for test

--- lesson_02/lesson_Car/src/main/java/com/betrybe/vehicles/App.java
+++ lesson_02/lesson_Car/src/main/java/com/betrybe/vehicles/App.java
@@ -2,4 +2,12 @@ package com.betrybe.vehicles;
 
 public class App {
 
+  public static void main(String[] args) {
+    System.out.println("Iniciando aplicação...");
+    System.out.println("A quantidade de carros instanciados é: " + Car.quantidadeDeCarros());
+    Car parati = new Car("wolksvaguem", "parati", 1978);
+    Car hondaCivic = new Car("Honda", "Civic", 2012);
+    parati.exibirInformacoes();
+    System.out.println("A quantidade de carros instanciados é: " + Car.quantidadeDeCarros());
+  }
 }
