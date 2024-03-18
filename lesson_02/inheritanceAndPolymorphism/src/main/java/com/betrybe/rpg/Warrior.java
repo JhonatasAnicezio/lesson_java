edd406670c871f648dd504895e9d3615060cfe73
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-18 11:43:04 -0300

    feat: implements class warrior at weapon and extends PlayableCharacter

--- lesson_02/inheritanceAndPolymorphism/src/main/java/com/betrybe/rpg/Warrior.java
+++ lesson_02/inheritanceAndPolymorphism/src/main/java/com/betrybe/rpg/Warrior.java
@@ -1,5 +1,13 @@
 package com.betrybe.rpg;
 
-public class Warrior {
+public class Warrior extends PlayableCharacter {
+  private String weapon; // arma
 
+  public String getWeapon() {
+    return weapon;
+  }
+
+  public void setWeapon(String weapon) {
+    this.weapon = weapon;
+  }
 }
