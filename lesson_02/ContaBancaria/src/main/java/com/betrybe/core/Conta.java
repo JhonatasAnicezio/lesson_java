7f1c599c27f921ab761d3d86da946a168f04a6aa
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-18 20:14:38 -0300

    feat: implements Conta class

--- lesson_02/ContaBancaria/src/main/java/com/betrybe/core/Conta.java
+++ lesson_02/ContaBancaria/src/main/java/com/betrybe/core/Conta.java
@@ -1,5 +1,17 @@
 package com.betrybe.core;
 
 public class Conta {
+  protected double saldo;
 
+  public double getSaldo() {
+    return saldo;
+  }
+
+  public void depositar(double valor) {
+    this.saldo += valor;
+  }
+
+  public void sacar(double valor) {
+    this.saldo -= valor;
+  }
 }
