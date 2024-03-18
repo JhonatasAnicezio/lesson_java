a7c2cf242831d4d9839a55f006aca6d50b85f0bc
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-18 15:32:53 -0300

    feat: implements class main by at instantiating Cleric class

--- lesson_02/inheritanceAndPolymorphism/src/main/java/com/betrybe/rpg/Main.java
+++ lesson_02/inheritanceAndPolymorphism/src/main/java/com/betrybe/rpg/Main.java
@@ -3,17 +3,22 @@ package com.betrybe.rpg;
 public class Main {
 
   public static void main(String[] args) {
-    Warrior warrior = new Warrior();
+//    Warrior warrior = new Warrior();
+//    Cleric cleric = new Cleric();
+//    Barbarian barbarian = new Barbarian();
+//
+//    moveAndAttack(warrior);
+//    moveAndAttack(cleric);
+//    moveAndAttack(barbarian);
+
     Cleric cleric = new Cleric();
-    Barbarian barbarian = new Barbarian();
-
-    moveAndAttack(warrior);
-    moveAndAttack(cleric);
-    moveAndAttack(barbarian);
+    cleric.heal();
+    cleric.heal("Porção de vida");
+    cleric.heal("Feitiço de luz", 7);
   }
 
-  public static void moveAndAttack(PlayableCharacter player) {
-    player.move();
-    player.attack();
-  }
+//  public static void moveAndAttack(PlayableCharacter player) {
+//    player.move();
+//    player.attack();
+//  }
 }
