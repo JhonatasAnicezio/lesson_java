c5e54b1bb710f1284a5d89bfc298a1df28a0f936
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-18 20:14:12 -0300

    fix: remove implementation in ContaPoupanca

--- lesson_02/ContaBancaria/src/main/java/com/betrybe/core/ContaPoupanca.java
+++ lesson_02/ContaBancaria/src/main/java/com/betrybe/core/ContaPoupanca.java
@@ -1,17 +1,5 @@
 package com.betrybe.core;
 
 public class ContaPoupanca {
-  protected double saldo;
 
-  public double getSaldo() {
-    return saldo;
-  }
-
-  public void depositar(double valor) {
-    this.saldo += valor;
-  }
-
-  public void sacar(double valor) {
-    this.saldo -= valor;
-  }
 }
