b5450e67d36c4a53ce72264a22e85ea4c785585f
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-18 14:39:46 -0300

    feat: execute method moveAndAttack

--- lesson_02/inheritanceAndPolymorphism/src/main/java/com/betrybe/rpg/Main.java
+++ lesson_02/inheritanceAndPolymorphism/src/main/java/com/betrybe/rpg/Main.java
@@ -4,15 +4,13 @@ public class Main {
 
   public static void main(String[] args) {
     Warrior warrior = new Warrior();
-    warrior.move();
-    warrior.attack();
+    Cleric cleric = new Cleric();
 
-    Mage mage = new Mage();
-    mage.move();
-    mage.attack();
+    moveAndAttack(warrior);
+    moveAndAttack(cleric);
   }
 
-  public void moveAndAttack(PlayableCharacter player) {
+  public static void moveAndAttack(PlayableCharacter player) {
     player.move();
     player.attack();
   }
