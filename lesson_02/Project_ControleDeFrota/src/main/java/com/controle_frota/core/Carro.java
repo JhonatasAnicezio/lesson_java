27d01c2d6e685260af83c19fd55c1836ba837922
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-19 15:37:56 -0300

    feat: implements Carro class

--- lesson_02/Project_ControleDeFrota/src/main/java/com/controle_frota/core/Carro.java
+++ lesson_02/Project_ControleDeFrota/src/main/java/com/controle_frota/core/Carro.java
@@ -1,5 +1,13 @@
 package com.controle_frota.core;
 
-public class Carro {
+public class Carro extends Veiculo {
 
+  public Carro(String placa, int anoFabricacao) {
+    super(placa, anoFabricacao);
+    this.setCombustivel("Flex");
+  }
+  @Override
+  public void abastecer(String local, String motorista, double valor) {
+    this.getAbastecimento().add(String.format("Local: %s, Motorista: %s, Valor: %d", local, motorista, valor));
+  }
 }
