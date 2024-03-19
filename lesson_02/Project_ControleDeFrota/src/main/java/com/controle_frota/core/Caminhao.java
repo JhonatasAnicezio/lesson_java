2c9828c3b5d2033ffd6fe452904e263ec04132e8
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-19 15:58:42 -0300

    feat: implements Transportador in Caminhao

--- lesson_02/Project_ControleDeFrota/src/main/java/com/controle_frota/core/Caminhao.java
+++ lesson_02/Project_ControleDeFrota/src/main/java/com/controle_frota/core/Caminhao.java
@@ -1,6 +1,6 @@
 package com.controle_frota.core;
 
-public class Caminhao extends Veiculo {
+public class Caminhao extends Veiculo implements Transportador {
 
   protected Caminhao(String placa, int anoFabricacao) {
     super(placa, anoFabricacao);
@@ -10,5 +10,10 @@ public class Caminhao extends Veiculo {
   @Override
   public void abastecer(String local, String motorista, double valor) {
     this.getAbastecimento().add(String.format("Local: base, Motorista: %s, Valor: %d", motorista, valor));
+  }
+
+  @Override
+  public String transportar(String origem, String destino) {
+    return null;
   }
 }
