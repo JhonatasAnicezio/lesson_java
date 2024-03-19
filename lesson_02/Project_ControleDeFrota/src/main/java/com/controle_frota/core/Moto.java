2355da7605cc8fd908e3648cc517db7ad7c5070b
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-19 16:06:13 -0300

    feat: implements Moto class

--- lesson_02/Project_ControleDeFrota/src/main/java/com/controle_frota/core/Moto.java
+++ lesson_02/Project_ControleDeFrota/src/main/java/com/controle_frota/core/Moto.java
@@ -1,5 +1,14 @@
 package com.controle_frota.core;
 
-public class Moto {
+public class Moto extends Veiculo {
 
+  protected Moto(String placa, int anoFabricacao) {
+    super(placa, anoFabricacao);
+    this.setCombustivel("Gasolina");
+  }
+
+  @Override
+  public void abastecer(String local, String motorista, double valor) {
+    this.getAbastecimento().add(String.format("Local: %s, Motorista: %s, Valor: %d", local, motorista, valor));
+  }
 }
