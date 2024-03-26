cdc8a4d47a4c196438cee115abc5132f88f99660
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-26 15:40:38 -0300

    feat: add anotation in ProductService at by spring maked inject dependency

--- lesson_03/product/src/main/java/com/betrybe/product/ProductService.java
+++ lesson_03/product/src/main/java/com/betrybe/product/ProductService.java
@@ -1,8 +1,14 @@
 package com.betrybe.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductService {
+  private ProductRepository productRepository;
 
+  @Autowired
+  public ProductService(ProductRepository productRepository) {
+    this.productRepository = productRepository;
+  }
 }
