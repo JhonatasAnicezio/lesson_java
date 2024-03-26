f0a8e2a28fc74d315673a36d93ad8ffea92dee52
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-26 07:09:52 -0300

    feat: implements controller

--- lesson_03/meu-projeto-spring/src/main/java/com/betrybe/meuprojetospring/controller/OlaMundoController.java
+++ lesson_03/meu-projeto-spring/src/main/java/com/betrybe/meuprojetospring/controller/OlaMundoController.java
@@ -1,5 +1,14 @@
 package com.betrybe.meuprojetospring.controller;
 
-public class OlaMundoController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/ola")
+public class OlaMundoController {
+  @GetMapping
+  public String dizOla() {
+    return "Hello, world!";
+  }
 }
