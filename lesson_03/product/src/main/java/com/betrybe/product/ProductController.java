76f446e724fcfbd14b91e04148f38ccd0ab8adea
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-26 15:27:13 -0300

    feat: implements ProductController at ProductService

--- lesson_03/product/src/main/java/com/betrybe/product/ProductController.java
+++ lesson_03/product/src/main/java/com/betrybe/product/ProductController.java
@@ -1,5 +1,9 @@
 package com.betrybe.product;
 
 public class ProductController {
+  private ProductService productService;
 
+  public ProductController(ProductService productService) {
+    this.productService = productService;
+  }
 }
