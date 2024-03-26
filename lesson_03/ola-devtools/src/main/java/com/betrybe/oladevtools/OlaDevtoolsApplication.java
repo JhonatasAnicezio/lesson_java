54313be39690f4309cdb9f3acfd9548040bccca9
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-26 07:57:53 -0300

    feat: create project java spring boot for lesson devtools

---
+++ lesson_03/ola-devtools/src/main/java/com/betrybe/oladevtools/OlaDevtoolsApplication.java
@@ -0,0 +1,13 @@
+package com.betrybe.oladevtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlaDevtoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlaDevtoolsApplication.class, args);
+	}
+
+}
---
+++ lesson_03/ola-devtools/src/test/java/com/betrybe/oladevtools/OlaDevtoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betrybe.oladevtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlaDevtoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
