3b030db187f4d03771025277e2f65a17d48e22ea
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-26 13:21:17 -0300

    feat: implements and test HotReload

--- lesson_03/java-web/src/main/java/com/betrybe/javaweb/JavaWebController.java
+++ lesson_03/java-web/src/main/java/com/betrybe/javaweb/JavaWebController.java
@@ -1,5 +1,14 @@
 package com.betrybe.javaweb;
 
-public class JavaWebController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/javaweb")
+public class JavaWebController {
+  @GetMapping
+  public String logDefault() {
+    return "Estou aprendendo a criar aplicações Java para Web!";
+  }
 }
