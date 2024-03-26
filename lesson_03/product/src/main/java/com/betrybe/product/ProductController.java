ddc81e670d7d5dc1561ac3b8e55ff1dbb497a1a7
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-26 15:32:37 -0300

    feat: implements anotations for inject dependency

--- lesson_03/product/src/main/java/com/betrybe/product/ProductController.java
+++ lesson_03/product/src/main/java/com/betrybe/product/ProductController.java
@@ -1,8 +1,13 @@
 package com.betrybe.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ProductController {
   private ProductService productService;
 
+  @Autowired
   public ProductController(ProductService productService) {
     this.productService = productService;
   }
