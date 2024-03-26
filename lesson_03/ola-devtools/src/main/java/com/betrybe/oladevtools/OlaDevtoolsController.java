f1b61c68c4341d377f771db8fa54e9e72075a66f
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-26 12:34:48 -0300

    feat: alter return for test at by dev tools

--- lesson_03/ola-devtools/src/main/java/com/betrybe/oladevtools/OlaDevtoolsController.java
+++ lesson_03/ola-devtools/src/main/java/com/betrybe/oladevtools/OlaDevtoolsController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class OlaDevtoolsController {
   @GetMapping
   public String hello() {
-    return "Olá, Dev tools";
+    return "Olá, Dev tools!!";
   }
 }
