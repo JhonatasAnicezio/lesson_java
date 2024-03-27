794076c629d01c0d856501fb7c1159646d3fe250
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-27 14:54:36 -0300

    feat: create method searchPodcast at by anotation RequestParam

--- lesson_03/podcast-manager/src/main/java/com/betrybe/podcastmanager/controller/PodcastController.java
+++ lesson_03/podcast-manager/src/main/java/com/betrybe/podcastmanager/controller/PodcastController.java
@@ -1,8 +1,10 @@
 package com.betrybe.podcastmanager.controller;
 
+import jakarta.websocket.server.PathParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +19,10 @@ public class PodcastController {
   @GetMapping("/{id}")
   public String getPodcast(@PathVariable int id) {
     return "Você pediu pelo podcast de id: %d".formatted(id);
+  }
+
+  @GetMapping("/search")
+  public String searchPodcast(@RequestParam String title) {
+    return "Você pesquisou pelo podcast de titulo: %s".formatted(title);
   }
 }
