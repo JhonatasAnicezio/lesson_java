8b63546a4b0b30dfa5ee6afae61fc2d2aac32e7e
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-27 16:06:11 -0300

    feat: implements PodcastService class

--- lesson_03/podcast-manager/src/main/java/com/betrybe/podcastmanager/service/PodcastService.java
+++ lesson_03/podcast-manager/src/main/java/com/betrybe/podcastmanager/service/PodcastService.java
@@ -1,5 +1,16 @@
 package com.betrybe.podcastmanager.service;
 
-public class PodcastService {
+import com.betrybe.podcastmanager.model.Podcast;
+import org.springframework.stereotype.Service;
 
+@Service
+public class PodcastService {
+  public Podcast findPodcastById(long id) {
+    Podcast podcast = new Podcast();
+    podcast.setId(id);
+    podcast.setName("Meu podcast");
+    podcast.setUrl("http://meupodcast.com.br");
+
+    return podcast;
+  }
 }
