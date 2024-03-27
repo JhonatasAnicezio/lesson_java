3ad972486a47c8e0f7cde2a4b9c9d6e1e55a84f8
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-27 14:14:38 -0300

    feat: implements PodcastController class

--- lesson_03/podcast-manager/src/main/java/com/betrybe/podcastmanager/controller/PodcastController.java
+++ lesson_03/podcast-manager/src/main/java/com/betrybe/podcastmanager/controller/PodcastController.java
@@ -1,5 +1,16 @@
 package com.betrybe.podcastmanager.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/podcasts")
 public class PodcastController {
+
+  @GetMapping
+  public String getRoot() {
+    return "Yay Podcasts!";
+  }
 
 }
