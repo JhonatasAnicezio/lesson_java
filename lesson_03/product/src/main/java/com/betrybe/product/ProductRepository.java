1951fc1ab8e8dea4a13baa688112e46ae067648a
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-27 07:20:52 -0300

    feat: implements database in ProductRepository class

--- lesson_03/product/src/main/java/com/betrybe/product/ProductRepository.java
+++ lesson_03/product/src/main/java/com/betrybe/product/ProductRepository.java
@@ -5,6 +5,11 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class ProductRepository {
+  private DataBase dataBase;
+
+  public ProductRepository(DataBase dataBase) {
+    this.dataBase = dataBase;
+  }
   public String getMessage() {
     return "Mensagem: produto retornado";
   }
