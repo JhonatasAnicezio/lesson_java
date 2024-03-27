fc1b63d3f646de92fa203bac754102e770839496
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-27 14:06:36 -0300

    feat: create project podcast-manager for lesson API REST

---
+++ lesson_03/podcast-manager/src/test/java/com/betrybe/podcastmanager/PodcastManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betrybe.podcastmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodcastManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
