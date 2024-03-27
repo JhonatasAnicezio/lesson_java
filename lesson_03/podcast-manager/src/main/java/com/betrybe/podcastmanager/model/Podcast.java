1b983d8bf056bfe63c49a1ebcaef0d747167e19a
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-27 15:39:15 -0300

    feat: implements Podcast class1

--- lesson_03/podcast-manager/src/main/java/com/betrybe/podcastmanager/model/Podcast.java
+++ lesson_03/podcast-manager/src/main/java/com/betrybe/podcastmanager/model/Podcast.java
@@ -1,5 +1,31 @@
 package com.betrybe.podcastmanager.model;
 
 public class Podcast {
+  private long id;
+  private String name;
+  private String url;
 
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
 }
