08f86066eace4cdc6af3e61876accd4facce7ffc
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-27 07:25:38 -0300

    feat: implements ProductConfig class at instantianting class DataBase

--- lesson_03/product/src/main/java/com/betrybe/product/ProductConfig.java
+++ lesson_03/product/src/main/java/com/betrybe/product/ProductConfig.java
@@ -1,5 +1,13 @@
 package com.betrybe.product;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ProductConfig {
 
+  @Bean
+  public DataBase primatyDatabase() {
+    return new DataBase("localhost");
+  }
 }
