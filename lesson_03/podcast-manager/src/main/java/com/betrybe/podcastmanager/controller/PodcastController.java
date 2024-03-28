56a05e5d695db09aa582ec1b07c9f541cb8dab2e
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-28 09:20:11 -0300

    feat: create createPodcast method Post

--- lesson_03/podcast-manager/src/main/java/com/betrybe/podcastmanager/controller/PodcastController.java
+++ lesson_03/podcast-manager/src/main/java/com/betrybe/podcastmanager/controller/PodcastController.java
@@ -4,9 +4,11 @@ import com.betrybe.podcastmanager.model.Podcast;
 import com.betrybe.podcastmanager.service.PodcastService;
 import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +40,12 @@ public class PodcastController {
     podcast.setUrl("http://meupodcast.com.br");
 
     return ResponseEntity.ok(podcast);
+  }
+
+  public ResponseEntity<Podcast> createPodcast(@RequestBody Podcast newPodcast) {
+    Podcast savedPodcast = podcastService.savePodcast(newPodcast);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(savedPodcast);
   }
 
   @GetMapping("/search")
