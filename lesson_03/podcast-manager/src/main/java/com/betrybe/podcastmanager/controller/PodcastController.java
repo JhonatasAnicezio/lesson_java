0f851a66217ab92440ff30c32d5322b93aa97976
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-03-28 09:21:34 -0300

    feat: add anotation PostMapping

--- lesson_03/podcast-manager/src/main/java/com/betrybe/podcastmanager/controller/PodcastController.java
+++ lesson_03/podcast-manager/src/main/java/com/betrybe/podcastmanager/controller/PodcastController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -42,6 +43,7 @@ public class PodcastController {
     return ResponseEntity.ok(podcast);
   }
 
+  @PostMapping
   public ResponseEntity<Podcast> createPodcast(@RequestBody Podcast newPodcast) {
     Podcast savedPodcast = podcastService.savePodcast(newPodcast);
 
