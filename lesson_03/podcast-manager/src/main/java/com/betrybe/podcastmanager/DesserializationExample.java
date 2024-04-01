19544b06e827ff8a54148af4190f2f5bdfbdae35
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-04-01 13:51:48 -0300

    feat: implements DesserializationExample class

--- lesson_03/podcast-manager/src/main/java/com/betrybe/podcastmanager/DesserializationExample.java
+++ lesson_03/podcast-manager/src/main/java/com/betrybe/podcastmanager/DesserializationExample.java
@@ -1,5 +1,20 @@
 package com.betrybe.podcastmanager;
 
+import com.betrybe.podcastmanager.model.Podcast;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class DesserializationExample {
 
+  public static void main(String[] args) throws JsonProcessingException {
+    String json = "{\"id\": 123456789,\"name\": \"podcast01\","
+        + " \"url\": \"https://podcast01.com\"}";
+
+    ObjectMapper mapper = new ObjectMapper();
+
+    Podcast podcast = mapper.readValue(json, Podcast.class);
+
+    System.out.println(podcast.getName());
+    System.out.println(podcast.getUrl());
+  }
 }
