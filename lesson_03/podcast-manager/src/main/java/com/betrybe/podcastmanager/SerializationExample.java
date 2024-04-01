1ad7ef9cd3a63517d2851f848318cd1bce19d3a2
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-04-01 13:44:49 -0300

    feat: implements SerializationExample

--- lesson_03/podcast-manager/src/main/java/com/betrybe/podcastmanager/SerializationExample.java
+++ lesson_03/podcast-manager/src/main/java/com/betrybe/podcastmanager/SerializationExample.java
@@ -1,5 +1,28 @@
 package com.betrybe.podcastmanager;
 
+import com.betrybe.podcastmanager.Dto.PodcastDto;
+import com.betrybe.podcastmanager.model.Podcast;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class SerializationExample {
 
+  public static void main(String[] args) throws JsonProcessingException {
+    Podcast podcast = new Podcast();
+
+    podcast.setId(123456789);
+    podcast.setName("Podcast 01");
+    podcast.setUrl("https://podcast01.com.br");
+
+    PodcastDto podcastDto = new PodcastDto(
+        podcast.getId(),
+        podcast.getName(),
+        podcast.getUrl()
+    );
+
+    ObjectMapper mapper = new ObjectMapper();
+    String json = mapper.writeValueAsString(podcastDto);
+    System.out.println(json);
+  }
 }
