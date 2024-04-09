620e19f7a3a12483e46601733853117c261d8791
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-04-09 15:44:45 -0300

    feat: create project for lesson API Rest by lesson video

---
+++ lesson_03/APTRest/src/main/java/com/betrybe/APTRest/AptRestApplication.java
@@ -0,0 +1,13 @@
+package com.betrybe.APTRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AptRestApplication.class, args);
+	}
+
+}
---
+++ lesson_03/APTRest/src/test/java/com/betrybe/APTRest/AptRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betrybe.APTRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AptRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
