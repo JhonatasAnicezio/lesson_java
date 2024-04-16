cc92dd7722b5e8980cd7eab62708e039a8f0fbcb
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-04-16 13:47:46 -0300

    feat: refact name

--- lesson_03/APTRest/src/main/java/com/betrybe/healthproviders/AptRestApplication.java
+++ lesson_03/APTRest/src/main/java/com/betrybe/healthproviders/AptRestApplication.java
@@ -1,4 +1,4 @@
-package com.betrybe.APTRest;
+package com.betrybe.healthproviders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- lesson_03/APTRest/src/test/java/com/betrybe/healthproviders/AptRestApplicationTests.java
+++ lesson_03/APTRest/src/test/java/com/betrybe/healthproviders/AptRestApplicationTests.java
@@ -1,4 +1,4 @@
-package com.betrybe.APTRest;
+package com.betrybe.healthproviders;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
