dfffa2570167e231ea55ac3be40b8f057a1e1904
Jhonatas <jhonatasanic@hotmail.com>
2024-05-14 15:24:57 -0300

    feat: implements MedicController in layer controller

--- lesson_03/APTRest/src/main/java/com/betrybe/healthproviders/controller/MedicController.java
+++ lesson_03/APTRest/src/main/java/com/betrybe/healthproviders/controller/MedicController.java
@@ -1,4 +1,11 @@
 package com.betrybe.healthproviders.controller;
 
+import com.betrybe.healthproviders.service.MedicService;
+
 public class MedicController {
+    private final MedicService medicService;
+
+    public MedicController(MedicService medicService) {
+        this.medicService = medicService;
+    }
 }
