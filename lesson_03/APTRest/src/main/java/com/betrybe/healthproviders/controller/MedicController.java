d9d27a7ed8b9cd7a394cf29973cafcecdfb8163f
Jhonatas <jhonatasanic@hotmail.com>
2024-05-14 15:36:04 -0300

    feat: add annotation RestController in layer controller

--- lesson_03/APTRest/src/main/java/com/betrybe/healthproviders/controller/MedicController.java
+++ lesson_03/APTRest/src/main/java/com/betrybe/healthproviders/controller/MedicController.java
@@ -1,10 +1,14 @@
 package com.betrybe.healthproviders.controller;
 
 import com.betrybe.healthproviders.service.MedicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class MedicController {
     private final MedicService medicService;
 
+    @Autowired
     public MedicController(MedicService medicService) {
         this.medicService = medicService;
     }
