0a8c19991f1b9a82be25f7a613e8d33e19e91815
Jhonatas <jhonatasanic@hotmail.com>
2024-05-14 16:04:26 -0300

    feat: create route Get and funciton getAllMedics in layers controller and service

--- lesson_03/APTRest/src/main/java/com/betrybe/healthproviders/controller/MedicController.java
+++ lesson_03/APTRest/src/main/java/com/betrybe/healthproviders/controller/MedicController.java
@@ -1,15 +1,28 @@
 package com.betrybe.healthproviders.controller;
 
+import com.betrybe.healthproviders.entity.Medic;
 import com.betrybe.healthproviders.service.MedicService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/medics")
 public class MedicController {
     private final MedicService medicService;
 
     @Autowired
     public MedicController(MedicService medicService) {
         this.medicService = medicService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Medic>> getMedics() {
+        List<Medic> medics = medicService.getAllMedics();
+        return ResponseEntity.ok(medics);
     }
 }
--- lesson_03/APTRest/src/main/java/com/betrybe/healthproviders/service/MedicService.java
+++ lesson_03/APTRest/src/main/java/com/betrybe/healthproviders/service/MedicService.java
@@ -1,8 +1,11 @@
 package com.betrybe.healthproviders.service;
 
+import com.betrybe.healthproviders.entity.Medic;
 import com.betrybe.healthproviders.repository.MedicFakeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MedicService {
@@ -11,5 +14,9 @@ public class MedicService {
     @Autowired
     public MedicService(MedicFakeRepository medicFakeRepository) {
         this.medicFakeRepository = medicFakeRepository;
+    }
+
+    public List<Medic> getAllMedics() {
+        return medicFakeRepository.getAllMedics();
     }
 }
