4c2c5ed1ea735872da18f35635afb09b454ca44f
Jhonatas <jhonatasanic@hotmail.com>
2024-05-14 15:26:00 -0300

    feat: maked MediFakeRepository in component spring

--- lesson_03/APTRest/src/main/java/com/betrybe/healthproviders/repository/MedicFakeRepository.java
+++ lesson_03/APTRest/src/main/java/com/betrybe/healthproviders/repository/MedicFakeRepository.java
@@ -1,13 +1,16 @@
 package com.betrybe.healthproviders.repository;
 
 import com.betrybe.healthproviders.entity.Medic;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Component
 public class MedicFakeRepository {
+
     private static final Map<Long, Medic> MEDICS = new HashMap<>();
 
     static {
