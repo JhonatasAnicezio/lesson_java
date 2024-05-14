bc261c07b89ef63e17f9a7cf82b137c07a926a23
Jhonatas <jhonatasanic@hotmail.com>
2024-05-14 15:27:03 -0300

    feat: implements annotations in Service for inject dependency

--- lesson_03/APTRest/src/main/java/com/betrybe/healthproviders/service/MedicService.java
+++ lesson_03/APTRest/src/main/java/com/betrybe/healthproviders/service/MedicService.java
@@ -1,10 +1,14 @@
 package com.betrybe.healthproviders.service;
 
 import com.betrybe.healthproviders.repository.MedicFakeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MedicService {
     private final MedicFakeRepository medicFakeRepository;
 
+    @Autowired
     public MedicService(MedicFakeRepository medicFakeRepository) {
         this.medicFakeRepository = medicFakeRepository;
     }
