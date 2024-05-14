eda5717e0910aa24349fece5b02fe621e641a5b9
Jhonatas <jhonatasanic@hotmail.com>
2024-05-14 15:20:18 -0300

    feat: implements MedicService class in service

--- lesson_03/APTRest/src/main/java/com/betrybe/healthproviders/service/MedicService.java
+++ lesson_03/APTRest/src/main/java/com/betrybe/healthproviders/service/MedicService.java
@@ -1,4 +1,11 @@
 package com.betrybe.healthproviders.service;
 
+import com.betrybe.healthproviders.repository.MedicFakeRepository;
+
 public class MedicService {
+    private final MedicFakeRepository medicFakeRepository;
+
+    public MedicService(MedicFakeRepository medicFakeRepository) {
+        this.medicFakeRepository = medicFakeRepository;
+    }
 }
