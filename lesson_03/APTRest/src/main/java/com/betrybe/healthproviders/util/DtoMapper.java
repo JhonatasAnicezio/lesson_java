c6ad6f48e3f2714046221540be74f5e1026f4fc0
Jhonatas <jhonatasanic@hotmail.com>
2024-05-14 22:54:56 -0300

    feat: implements MedicDto class

--- lesson_03/APTRest/src/main/java/com/betrybe/healthproviders/util/DtoMapper.java
+++ lesson_03/APTRest/src/main/java/com/betrybe/healthproviders/util/DtoMapper.java
@@ -1,4 +1,14 @@
 package com.betrybe.healthproviders.util;
 
+import com.betrybe.healthproviders.dto.MedicDto;
+import com.betrybe.healthproviders.entity.Medic;
+
 public class DtoMapper {
+    public static MedicDto entityDto(Medic medic) {
+        return new MedicDto(
+                medic.getId(),
+                medic.getName(),
+                medic.getSpecialty()
+        );
+    }
 }
