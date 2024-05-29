8b40b160645e34d469e1b35c4061bfda565e6289
Jhonatas <jhonatasanic@hotmail.com>
2024-05-29 13:15:08 -0300

    feat: create test for method dividir

--- lesson_03/Calculator/src/test/java/org/example/CalculadoraTest.java
+++ lesson_03/Calculator/src/test/java/org/example/CalculadoraTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,9 +16,19 @@ class CalculadoraTest {
     assertEquals(34, cal.somar(32, 2));
   }
 
-  @Test@DisplayName("Teste do método subtrair")
+  @Test
+  @DisplayName("Teste do método subtrair")
   public void testeSubtrais() {
     Calculadora cal = new Calculadora();
     assertEquals(1, cal.subtrair(3, 2));
+  }
+
+  @Test
+  @DisplayName("Teste de exceção do método dividir")
+  public void testeDividirExceção() {
+    Calculadora cal = new Calculadora();
+    assertThrows(ArithmeticException.class, () -> {
+      cal.dividir(3, 0);
+    });
   }
 }
