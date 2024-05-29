886f549115a965185b8ca4df321e6adc5ee81ac4
Jhonatas <jhonatasanic@hotmail.com>
2024-05-29 12:59:46 -0300

    feat: add anotation in test

--- lesson_03/Calculator/src/test/java/org/example/CalculadoraTest.java
+++ lesson_03/Calculator/src/test/java/org/example/CalculadoraTest.java
@@ -2,17 +2,20 @@ package org.example;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Testes da classe Calculadora")
 class CalculadoraTest {
 
   @Test
+  @DisplayName("Teste do método somar")
   public void testeSomar() {
     Calculadora cal = new Calculadora();
     assertEquals(34, cal.somar(32, 2));
   }
 
-  @Test
+  @Test@DisplayName("Teste do método subtrair")
   public void testeSubtrais() {
     Calculadora cal = new Calculadora();
     assertEquals(1, cal.subtrair(3, 2));
