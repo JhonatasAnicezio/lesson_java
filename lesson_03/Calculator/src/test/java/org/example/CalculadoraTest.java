19a1cc21cc472cbe35d4c35dbe8632940de6f104
Jhonatas <jhonatasanic@hotmail.com>
2024-05-29 12:10:05 -0300

    feat: implements CalculadoraTest class for testing

--- lesson_03/Calculator/src/test/java/org/example/CalculadoraTest.java
+++ lesson_03/Calculator/src/test/java/org/example/CalculadoraTest.java
@@ -1,5 +1,20 @@
 package org.example;
 
-public class CalculadoraTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
+class CalculadoraTest {
+
+  @Test
+  public void testeSomar() {
+    Calculadora cal = new Calculadora();
+    assertEquals(34, cal.somar(32, 2));
+  }
+
+  @Test
+  public void testeSubtrais() {
+    Calculadora cal = new Calculadora();
+    assertEquals(1, cal.subtrair(3, 2));
+  }
 }
