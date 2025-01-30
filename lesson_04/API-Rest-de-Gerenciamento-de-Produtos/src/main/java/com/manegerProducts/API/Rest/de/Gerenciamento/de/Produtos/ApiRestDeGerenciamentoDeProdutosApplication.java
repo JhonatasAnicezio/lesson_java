ae736b7bf70a2632b72d0e319a0c0cad880bb56f
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2025-01-30 11:25:16 -0300

    feat: create project for lesson

---
+++ lesson_04/API-Rest-de-Gerenciamento-de-Produtos/src/main/java/com/manegerProducts/API/Rest/de/Gerenciamento/de/Produtos/ApiRestDeGerenciamentoDeProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.manegerProducts.API.Rest.de.Gerenciamento.de.Produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestDeGerenciamentoDeProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestDeGerenciamentoDeProdutosApplication.class, args);
+	}
+
+}
---
+++ lesson_04/API-Rest-de-Gerenciamento-de-Produtos/src/test/java/com/manegerProducts/API/Rest/de/Gerenciamento/de/Produtos/ApiRestDeGerenciamentoDeProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manegerProducts.API.Rest.de.Gerenciamento.de.Produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestDeGerenciamentoDeProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
