44d68d1be024936d16e0746cc7d6ffff2d7280b1
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2025-02-08 10:23:30 -0300

    feat: create adviceController

--- lesson_04/persistencia_validacao/src/main/java/com/example/persistencia_validacao/advice/GlobalControllerAdvice.java
+++ lesson_04/persistencia_validacao/src/main/java/com/example/persistencia_validacao/advice/GlobalControllerAdvice.java
@@ -1,4 +1,19 @@
 package com.example.persistencia_validacao.advice;
 
+import com.example.persistencia_validacao.service.exception.ProductNotFound;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalControllerAdvice {
+
+  @ExceptionHandler({
+      ProductNotFound.class
+  })
+  public ResponseEntity<String> handleNotFound(Exception exception) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND)
+        .body(exception.getMessage());
+  }
 }
