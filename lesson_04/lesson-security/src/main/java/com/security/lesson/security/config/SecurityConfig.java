ebf203fe28501d0d7b56ae3f2b1f6bc6e1411dfb
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2025-02-17 19:18:36 -0300

    feat: create add filters

--- lesson_04/lesson-security/src/main/java/com/security/lesson/security/config/SecurityConfig.java
+++ lesson_04/lesson-security/src/main/java/com/security/lesson/security/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.security.lesson.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -22,6 +23,10 @@ public class SecurityConfig {
         .csrf(AbstractHttpConfigurer::disable)
         .sessionManagement(session ->
             session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+            .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+            .anyRequest().authenticated())
         .build();
   }
 
