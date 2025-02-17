fba140fe8d40973e22ef32500a6256ace14e0e0c
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2025-02-17 06:26:34 -0300

    start project security

---
+++ lesson_04/lesson-security/src/main/java/com/security/lesson/security/LessonSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.security.lesson.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonSecurityApplication.class, args);
+	}
+
+}
---
+++ lesson_04/lesson-security/src/test/java/com/security/lesson/security/LessonSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.lesson.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
